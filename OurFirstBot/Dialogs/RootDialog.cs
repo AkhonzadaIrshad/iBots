da5a9d75b9b8373607620d6299259a506a3d81f1
Irshad Ahmed <irshadahmed31@outlook.com>
2018-02-24 04:04:07 +0500

    Bot Application
    First Bot Application using Microsoft Bot Framework

---
+++ OurFirstBot/Dialogs/RootDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace OurFirstBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+
+            await context.PostAsync("Hello tell me your sweet name?");
+
+            context.Wait(NameToldAsync);
+        }
+
+        private async Task NameToldAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            string userName = activity.Text;
+            await context.PostAsync($"Hey {userName} welcome to Bot");
+            context.Wait(NameToldAsync);
+ 
+        }
+    }
+}
---
+++ OurFirstBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OurFirstBot.WebApiApplication" Language="C#" %>
